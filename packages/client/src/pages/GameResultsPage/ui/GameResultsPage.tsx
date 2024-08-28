2b81f548639ca1ceef506c5f5d6e00008a2f135b
Dina Pupina <NPupina@beeline.ru>
2024-08-28 18:47:32 +0300

    Правки по комментариям коллег :)

--- packages/client/src/pages/GameResultsPage/ui/GameResultsPage.tsx
+++ packages/client/src/pages/GameResultsPage/ui/GameResultsPage.tsx
@@ -5,13 +5,9 @@ import { NavLink } from 'react-router-dom'
 import { LogoWithModal } from '@/widgets/LogoWithModal'
 import { getPageUrl } from '@/shared/config/router/routerConfig'
 
-interface GameResultsPageProps {
-  test?: string
-}
-
-export const GameResultsPage = (props: GameResultsPageProps) => {
-  const result = '300 баллов' // Заглушка для результата, потом будем передавать результат игры
-  const bestResult = '1300 баллов' // Заглушка для лучшего результата, потом будем передавать лучший результат игры
+export const GameResultsPage = () => {
+  const result = '13:33' // Заглушка для результата, потом будем передавать результат игры
+  const bestResult = '12:01' // Заглушка для лучшего результата, потом будем передавать лучший результат игры
 
   return (
     <div className={classNames(cls.GameResultsPage)}>
@@ -23,27 +19,19 @@ export const GameResultsPage = (props: GameResultsPageProps) => {
         width={344}
         mask={false}
         transitionName={''}
-        title="Вы победили!">
+        title="Congratulation!">
         <Form className={cls.gameOverPageWrapper} layout="vertical">
-          <Form.Item className={cls.gameOverPageItem} label="Ваш результат:">
+          <Form.Item className={cls.gameOverPageItem} label="Your Result">
             <div className={cls.result}>{result}</div>
           </Form.Item>
 
-          <Form.Item
-            className={cls.gameOverPageItem}
-            label="Ваш лучший результат:">
+          <Form.Item className={cls.gameOverPageItem} label="Your Best Result">
             <div className={cls.bestResult}>{bestResult}</div>
           </Form.Item>
 
           <Form.Item className={cls.gameOverPageButton}>
             <NavLink to={getPageUrl('game')}>
-              <Button type="primary">Сыграть еще раз</Button>
-            </NavLink>
-          </Form.Item>
-
-          <Form.Item className={cls.gameOverPageButton}>
-            <NavLink to={getPageUrl('main')}>
-              <Button type="link">Главное меню</Button>
+              <Button type="primary">Play again</Button>
             </NavLink>
           </Form.Item>
         </Form>
