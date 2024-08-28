a46f9da6d34d734c31b74373ac7e9e09d39f73b6
Yuriy Dolganov <jetscript@gmail.com>
2024-08-28 10:00:44 +0500

    [client][userAvatar] rename styles to cls

--- packages/client/src/shared/ui/UserAvatar/UserAvatar.tsx
+++ packages/client/src/shared/ui/UserAvatar/UserAvatar.tsx
@@ -1,12 +1,10 @@
 import { type ComponentProps, type FC } from 'react'
 import { Avatar } from 'antd'
 import { UserOutlined } from '@ant-design/icons'
-import styles from './UserAvatar.module.scss'
+import cls from './UserAvatar.module.scss'
 
 type Props = Pick<ComponentProps<typeof Avatar>, 'src'>
 
 export const UserAvatar: FC<Props> = ({ src }) => {
-  return (
-    <Avatar className={styles.fullWidth} icon={<UserOutlined />} src={src} />
-  )
+  return <Avatar className={cls.fullWidth} icon={<UserOutlined />} src={src} />
 }
