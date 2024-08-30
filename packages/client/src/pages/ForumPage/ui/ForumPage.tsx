369757b701f6456c9235b30dad92e41a0b948383
legeidaa <andrey.ligiza@mail.ru>
2024-08-30 20:47:56 +0300

    feat: страницы форума и темы форума

--- packages/client/src/pages/ForumPage/ui/ForumPage.tsx
+++ packages/client/src/pages/ForumPage/ui/ForumPage.tsx
@@ -1,9 +1,63 @@
+import { LogoWithModal } from '@/widgets/LogoWithModal'
 import cls from './ForumPage.module.scss'
+import { Typography } from 'antd'
+import {
+  ForumPageStages,
+  forumsListMock,
+  forumTopicsListMock,
+} from '../model/forumData'
+import { useEffect, useState } from 'react'
+import { CreateNewThreadForm } from './CreateNewThreadForm/CreateNewThreadForm'
+import { ForumTopicsList } from './ForumTopicsList/ForumTopicsList'
+import { ForumsList } from './ForumsList/ForumsList'
 
-interface ForumPageProps {
-  test?: string
-}
+export const ForumPage = () => {
+  const [pageTitle, setPageTitle] = useState('Forums')
+  const [currentStage, setCurrentStage] = useState(ForumPageStages.forumsList)
 
-export const ForumPage = (props: ForumPageProps) => {
-  return <div className={cls.ForumPage}>ForumPage</div>
+  useEffect(() => {
+    if (currentStage === ForumPageStages.forumsList) {
+      setPageTitle('Forums')
+    }
+    if (currentStage === ForumPageStages.forumTopicsList) {
+      setPageTitle('Forum name')
+    }
+    if (currentStage === ForumPageStages.createThread) {
+      setPageTitle('Create New Thread')
+    }
+  }, [currentStage])
+
+  const changeStage = (stage: ForumPageStages) => {
+    setCurrentStage(stage)
+  }
+
+  return (
+    <LogoWithModal
+      open={true}
+      centered={true}
+      closable={false}
+      footer={null}
+      width={500}
+      mask={false}
+      transitionName={''}
+      title={
+        <Typography.Title level={3} className={cls.noMargin}>
+          {pageTitle}
+        </Typography.Title>
+      }>
+      {currentStage === ForumPageStages.forumsList && (
+        <ForumsList changeStage={changeStage} forumsList={forumsListMock} />
+      )}
+      {currentStage === ForumPageStages.forumTopicsList && (
+        <ForumTopicsList
+          changeStage={changeStage}
+          forumTopicsList={forumTopicsListMock}
+        />
+      )}
+
+      {currentStage === ForumPageStages.createThread && (
+        <CreateNewThreadForm changeStage={changeStage} />
+      )}
+    </LogoWithModal>
+  )
 }
