ee7db8dd65be9e138cd7a3b671c31508bc5838f3
Yuriy Dolganov <jetscript@gmail.com>
2024-09-01 18:16:23 +0500

    [client][utils] add isNull, isEven, isDefined, formatTime

---
+++ packages/client/src/shared/utils/formatTime.ts
@@ -0,0 +1,16 @@
+export function formatTime(milliseconds: number): string {
+  const totalSeconds = Math.floor(milliseconds / 1000)
+  const hours = Math.floor(totalSeconds / 3600)
+  const minutes = Math.floor((totalSeconds % 3600) / 60)
+  const seconds = totalSeconds % 60
+
+  if (hours > 0) {
+    return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)}`
+  } else {
+    return `${padZero(minutes)}:${padZero(seconds)}`
+  }
+}
+
+function padZero(num: number): string {
+  return num.toString().padStart(2, '0')
+}
---
+++ packages/client/src/shared/utils/index.ts
@@ -0,0 +1,4 @@
+export { formatTime } from './formatTime'
+export { isDefined } from './isDefined'
+export { isEven } from './isEven'
+export { isNull } from './isNull'
