be1df3ca13469a91fa2388b8b7a52b1fb58f3909
Бурнашова Ксения <k.burnashova@lenvendo.ru>
2024-09-09 11:07:03 +0300

    WAI-36: исправления для тестов

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,7 +1,5 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from '@/shared/store/store';
 import { App } from './app/App';
 import '@/shared/assets/themes/colors.scss';
 import '@/shared/assets/themes/styles.scss';
@@ -10,9 +8,7 @@ const rootNode = document.getElementById('root');
 if (rootNode) {
   createRoot(rootNode).render(
     <StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <App />
     </StrictMode>,
   );
 } else {
