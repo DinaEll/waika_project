8f0310d85517f5c07c6d5eb7810aec358572e11a
Бурнашова Ксения <k.burnashova@lenvendo.ru>
2024-09-10 12:11:30 +0300

    WAI-33: доработка сервис воркера

--- packages/client/cacheSW.js
+++ packages/client/cacheSW.js
@@ -1,10 +1,13 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call*/
-/* eslint-disable @typescript-eslint/no-unsafe-member-access*/
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 const VERSION = 'v1';
 const CACHE_NAME = `waika-project-cache-${VERSION}`;
 
 const URLS = [
   '/',
+  '/src/shared/assets/svg/main-logo.svg',
+  '/src/shared/assets/images/rules.png'
 ];
 
 self.addEventListener('install', event => {
@@ -12,58 +15,53 @@ self.addEventListener('install', event => {
     caches
       .open(CACHE_NAME)
       .then(cache => cache.addAll(URLS))
-      .catch(error => console.log(error))
+      .catch(error => {
+        console.log('SW cache error:', error);
+        throw error;
+      })
   );
-
-  event.waitUntil(self.skipWaiting());
 });
 
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            if (!cacheWhitelist.includes(cacheName)) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+  );
+});
 
-// self.addEventListener('activate', event => {
-//   event.waitUntil(self.clients.claim());
-//   event.waitUntil(
-//     caches
-//       .keys()
-//       .then(cachesNames => {
-//         return Promise.all(
-//           cachesNames
-//             .filter(name => name !== CACHE_NAME)
-//             .map(name => caches.delete(name))
-//         )
-//       })
-//   );
-// });
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then(async (response) => {
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
 
+        const responseToCache = response.clone();
+        await caches
+          .open(CACHE_NAME)
+          .then(async (cache) => {
+            if (event.request.url.startsWith('http') || event.request.url.startsWith('https')) {
+              await cache.put(event.request, responseToCache);
+            }
+          });
 
-// self.addEventListener('fetch', event => {
-//   event.respondWith(
-//     fetch(event.request)
-//       .then(async (response) => {
-//         if (!response || response.status !== 200 || response.type !== 'basic') {
-//           return response;
-//         }
-//
-//         const networkResponse = response.clone();
-//
-//         await caches
-//           .open(CACHE_NAME)
-//           .then(cache => {
-//             if (event.request.url.startsWith('http') || event.request.url.startsWith('https')) {
-//               cache.put(event.request, networkResponse);
-//             }
-//           });
-//
-//         return response;
-//       })
-//       .catch(async () => {
-//         console.log('Оффлайн');
-//
-//         return await caches
-//           .open(CACHE_NAME)
-//           .then(cache => cache.match(event.request))
-//           .then(response => response || Promise.reject('Нет данных в кеше'));
-//       })
-//   );
-// });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
 
-/* eslint-enable */
+/* eslint-enable @typescript-eslint/no-unsafe-call */
+/* eslint-enable @typescript-eslint/no-unsafe-argument */
+/* eslint-enable @typescript-eslint/no-unsafe-member-access */
