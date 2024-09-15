d6f18827b6a01d33729aa2643f516f0a05a48776
sergeyemelianov <semelianov@domprog.com>
2024-09-15 22:43:45 +0300

    WAI-32

--- packages/client/src/entities/mahjong/model/Mahjong.ts
+++ packages/client/src/entities/mahjong/model/Mahjong.ts
@@ -109,12 +109,6 @@ export class Mahjong extends Game {
     this.remainingShuffles -= 1;
   };
 
-  getRandomInt(min: number, max: number): number {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
   private createTileArray(tileIds: FieldCell[], x: number): void {
     tileIds.forEach((tileId, y) => {
       const imgSrc = `/src/shared/assets/game-tiles/MJ-${tileId}.svg`;
