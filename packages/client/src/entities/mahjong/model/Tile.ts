1b65aab6fe620ec81302680820d89e07a96ad845
sergeyemelianov <semelianov@domprog.com>
2024-09-15 22:49:03 +0300

    WAI-32

--- packages/client/src/entities/mahjong/model/Tile.ts
+++ packages/client/src/entities/mahjong/model/Tile.ts
@@ -31,12 +31,6 @@ export class Tile extends CanvasElement {
     this.props = props;
   }
 
-  getRandomInt(min: number, max: number): number {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
   public draw() {
     if (!this.isVisible) return;
 
