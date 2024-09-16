d5fd7078d5a1f1f7fb32eca0e9b15c46587c0d6f
Yuriy Dolganov <jetscript@gmail.com>
2024-09-16 13:06:03 +0500

    [client][hooks] optimize fullscreen hook

--- packages/client/src/shared/hooks/useFullscreen.ts
+++ packages/client/src/shared/hooks/useFullscreen.ts
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 interface FullscreenHook {
   isFullscreen: boolean;
@@ -13,24 +13,6 @@ export const useFullscreen = (): FullscreenHook => {
     setIsFullscreen(Boolean(document.fullscreenElement));
   };
 
-  const enterFullscreen = useCallback((element: HTMLElement) => {
-    if (element.requestFullscreen) {
-      element.requestFullscreen().catch((err) => {
-        alert('Error entering fullscreen mode:' + err);
-        console.error('Error entering fullscreen mode:', err);
-      });
-    }
-  }, []);
-
-  const exitFullscreen = useCallback(() => {
-    if (document.exitFullscreen) {
-      document.exitFullscreen().catch((err) => {
-        alert('Error exiting fullscreen mode:' + err);
-        console.error('Error exiting fullscreen mode:', err);
-      });
-    }
-  }, []);
-
   useEffect(() => {
     document.addEventListener('fullscreenchange', onFullscreenChange);
     return () => {
@@ -44,3 +26,21 @@ export const useFullscreen = (): FullscreenHook => {
     exitFullscreen,
   };
 };
+
+function enterFullscreen(element: HTMLElement) {
+  if (element.requestFullscreen) {
+    element.requestFullscreen().catch((err) => {
+      alert('Error entering fullscreen mode:' + err);
+      console.error('Error entering fullscreen mode:', err);
+    });
+  }
+}
+
+function exitFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen().catch((err) => {
+      alert('Error exiting fullscreen mode:' + err);
+      console.error('Error exiting fullscreen mode:', err);
+    });
+  }
+}
