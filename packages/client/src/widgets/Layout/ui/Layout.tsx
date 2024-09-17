ff86f74cd11ea1189c9f6b93b5bae82b8a670b7d
Yuriy Dolganov <jetscript@gmail.com>
2024-09-17 11:10:35 +0500

    [client][layout] remove old nav

--- packages/client/src/widgets/Layout/ui/Layout.tsx
+++ packages/client/src/widgets/Layout/ui/Layout.tsx
@@ -1,6 +1,5 @@
 import { unwrapResult } from '@reduxjs/toolkit';
-import classNames from 'classnames';
-import { Outlet, NavLink, useNavigate, useLocation } from 'react-router-dom';
+import { Outlet, useNavigate, useLocation } from 'react-router-dom';
 import { getPageUrl, pagesPaths } from '@/shared/config';
 import { useEffectOnce } from '@/shared/hooks';
 import { useAppDispatch } from '@/shared/store/hooks';
@@ -35,18 +34,6 @@ export const Layout = () => {
 
   return (
     <div className={cls.layout}>
-      <nav className={classNames(cls.navbar)}>
-        <NavLink to={getPageUrl('main')}>Main</NavLink>
-        <NavLink to={getPageUrl('registration')}>Registration</NavLink>
-        <NavLink to={getPageUrl('login')}>Login</NavLink>
-        <NavLink to={getPageUrl('forum')}>Forum</NavLink>
-        <NavLink to={getPageUrl('game')}>Game</NavLink>
-        <NavLink to={getPageUrl('leaderboard')}>Leaderboard</NavLink>
-        <NavLink to={getPageUrl('not-found')}>Not Found Page</NavLink>
-        <NavLink to={getPageUrl('server-error')}>Server Error Page</NavLink>
-        <NavLink to={getPageUrl('profile')}>Profile</NavLink>
-      </nav>
-
       <Header />
 
       <ErrorBoundary>
