9517bfdac53219ce4090a54beb9c96492d1764f5
Yuriy Dolganov <jetscript@gmail.com>
2024-09-17 20:40:39 +0500

    [client]fix auto merge

--- packages/client/src/widgets/Layout/ui/Layout.tsx
+++ packages/client/src/widgets/Layout/ui/Layout.tsx
@@ -1,6 +1,4 @@
-import classNames from 'classnames';
-import { Outlet, NavLink } from 'react-router-dom';
-import { getPageUrl } from '@/shared/config';
+import { Outlet } from 'react-router-dom';
 import { useEffectOnce } from '@/shared/hooks';
 import { useAppDispatch, useAppSelector } from '@/shared/store/hooks';
 import { fetchUser } from '@/shared/store/user/user.action';
@@ -18,18 +16,6 @@ export const Layout = () => {
 
   return (
     <div className={cls.layout}>
-      <nav className={classNames(cls.navbar)}>
-        <NavLink to={getPageUrl('main')}>Main</NavLink>
-        <NavLink to={getPageUrl('registration')}>Registration</NavLink>
-        <NavLink to={getPageUrl('login')}>Login</NavLink>
-        <NavLink to={getPageUrl('forum')}>Forum</NavLink>
-        <NavLink to={getPageUrl('game')}>Game</NavLink>
-        <NavLink to={getPageUrl('leaderboard')}>Leaderboard</NavLink>
-        <NavLink to={getPageUrl('not-found')}>Not Found Page</NavLink>
-        <NavLink to={getPageUrl('server-error')}>Server Error Page</NavLink>
-        <NavLink to={getPageUrl('profile')}>Profile</NavLink>
-      </nav>
-
       {user && <Header />}
 
       <ErrorBoundary>
