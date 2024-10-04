4f2063189c61c901dbbf09f3a5d71ca4172a7fc7
Yuriy Dolganov <jetscript@gmail.com>
2024-10-04 13:00:30 +0500

    [client][layout] fix, remove load user

--- packages/client/src/widgets/Layout/ui/Layout.tsx
+++ packages/client/src/widgets/Layout/ui/Layout.tsx
@@ -1,22 +1,17 @@
+import classNames from 'classnames';
+import { type FC } from 'react';
 import { Outlet } from 'react-router-dom';
-import { useEffectOnce } from '@/shared/hooks';
-import { useAppDispatch, useAppSelector } from '@/shared/store/hooks';
-import { fetchUser } from '@/shared/store/user/user.action';
-import { ErrorBoundary } from '@/widgets/ErrorBoundary/ErrorBoundary';
-import { Header } from '@/widgets/Header';
+import { useAppSelector } from '@/shared/store/hooks';
+import { isUserAuthSelector } from '@/shared/store/user/user.selector';
+import { ErrorBoundary, Header } from '@/widgets';
 import cls from './Layout.module.scss';
 
-export const Layout = () => {
-  const dispatch = useAppDispatch();
-  const user = useAppSelector((state) => state.user.data);
-
-  useEffectOnce(() => {
-    dispatch(fetchUser()).catch(console.error);
-  });
+export const Layout: FC = () => {
+  const isUserAuth = useAppSelector(isUserAuthSelector);
 
   return (
-    <div className={cls.layout}>
-      {user && <Header />}
+    <div className={classNames(cls.layout, isUserAuth && cls.layoutAuth)}>
+      {isUserAuth ? <Header /> : null}
 
       <ErrorBoundary>
         <main className={cls.content}>
