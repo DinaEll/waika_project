e8f329e68e3d453bc6500e2fd1f7b85b50e0ad38
Yuriy Dolganov <jetscript@gmail.com>
2024-10-12 19:07:34 +0500

    [server] fix wipe dist & add start info & fix test

--- packages/server/scripts/esbuild.watch.ts
+++ packages/server/scripts/esbuild.watch.ts
@@ -1,10 +1,12 @@
 import { context } from 'esbuild';
+import { SERVER_PORT } from '../env';
 import { esBuildConfig } from './esbuild.config';
 
 async function buildWatch() {
   const ctx = await context(esBuildConfig);
 
   await ctx.watch();
+  console.log(`  ➜ 🎸 Server is listening on: http://localhost:${SERVER_PORT}`);
 }
 
 buildWatch().catch((error) => {
