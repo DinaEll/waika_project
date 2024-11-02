fa4f673215ff7f2e319af55c0633f831d005ab6f
Yuriy Dolganov <jetscript@gmail.com>
2024-11-02 12:55:23 +0500

    remove ssr

--- packages/ssr/env.ts
+++
@@ -1,5 +0,0 @@
-import { cleanEnv, port } from 'envalid';
-
-export const { CLIENT_PORT, isDev } = cleanEnv(process.env, {
-  CLIENT_PORT: port({ devDefault: 3000 }),
-});
--- packages/ssr/src/types.ts
+++
@@ -1,6 +0,0 @@
-import type { Request, Response } from '@waika_project/server';
-
-export type EntryServerRender = (
-  req: Request,
-  res: Response,
-) => Promise<{ html: string; initialState: unknown }>;
