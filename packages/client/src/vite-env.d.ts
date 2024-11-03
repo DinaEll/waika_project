1bedc5294e150eaccefc62d776e2c9077eea3c10
Yuriy Dolganov <jetscript@gmail.com>
2024-11-03 18:09:07 +0500

    [client] fix env

--- packages/client/src/vite-env.d.ts
+++ packages/client/src/vite-env.d.ts
@@ -1,4 +1,6 @@
 /// <reference types="vite/client" />
 
+declare const __API_BASE_URL__: string;
+declare const __APP_NAME__: string;
 declare const __CLIENT_PORT__: number;
 declare const __isDev__: boolean;
