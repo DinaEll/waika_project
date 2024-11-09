6ef361f3f05e242478cabf8ce555e10cde51e122
Andrey Ligiza <andrey.ligiza@mail.ru>
2024-11-09 11:26:12 +0300

    refactor: удаление лишнего

--- packages/client/src/pages/ForumPage/model/forumData.ts
+++ packages/client/src/pages/ForumPage/model/forumData.ts
@@ -12,29 +12,6 @@ export interface ForumTopicData {
   replies: number;
   views: number;
 }
-export const forumTopicsListMock: ForumTopicData[] = [
-  {
-    id: 'doom777',
-    title: 'Doom 777',
-    author: 'Doom',
-    replies: 99,
-    views: 99,
-  },
-  {
-    id: 'sometopicname',
-    title: 'Mahjong by Шaika',
-    author: 'Dina',
-    replies: 0,
-    views: 0,
-  },
-  {
-    id: 'anothertopicname',
-    title: 'WoW: The War Within',
-    author: 'Vonkrieger',
-    replies: 111,
-    views: 99,
-  },
-];
 
 export const enum ForumPageStages {
   forumTopicsList = 'forumTopicsList',
